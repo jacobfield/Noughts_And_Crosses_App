bba754eb276ce74571b2d46f0ae16d82411a1fd5
Thomas Vincent <thomasvincent@live.co.uk>
2023-11-10 15:01:09 +0000

    function inputXOrO

--- x-o/src/Grid.js
+++ x-o/src/Grid.js
@@ -6,8 +6,23 @@ export function Grid() {
   // each grid cell is an invisible button and displays <p>{initialOXGrid.value</p>
   // Upon button click, set value to either X / O, dictated by a function
   const [grid, setGrid] = useState(initialOXGrid);
+  const [playerMove, setPlayerMove] = useState("X");
+
   //   console.log(initialOXGrid);
   // grid.value = what will be updated with setGrid
+
+  // function to set inputXOrO
+  // let playerMove = "X"
+  // When called, sets state of value to playerMove
+  // if playerMove === "X" ? playerMove = "O" : playerMove = "X"
+  function inputXOrO(gridData, playerMove) {
+    // set state of playerMove - gridData.value = playerMove
+    gridData.value = playerMove;
+    // change value o playerMove from X to O or O to X
+    playerMove === "X" ? setPlayerMove("O") : setPlayerMove("X");
+  }
+
+  // on grid-item button, have onClick={inputXOrO}
 
   return (
     <div className="gameGrid">
@@ -28,10 +43,3 @@ function GridItem({ id, value }) {
   console.log(id);
   return <button className="grid-item">{value}</button>;
 }
-
-// function to set inputXOrO
-// let playerMove = "X"
-// When called, sets state of value to playerMove
-// if playerMove === "X" ? playerMove = "O" : playerMove = "X"
-
-// on grid-item button, have onClick={inputXOrO}
