2dae089b1d442b2bc6a580039323770e04d02f66
jacobfield <jacobprogrammes@gmail.com>
2023-11-10 16:18:38 +0000

    stylings

--- x-o/src/Header.js
+++ x-o/src/Header.js
@@ -1,8 +1,13 @@
 export function Header() {
+  const refreshPage = () => {
+    window.location.reload();
+  };
   return (
     <header className="headerClass">
       <h1>Team 19 Noughts & Crosses</h1>
-      <p></p>
+      <button className="restartButton" onClick={refreshPage}>
+        Restart?
+      </button>
     </header>
   );
 }
