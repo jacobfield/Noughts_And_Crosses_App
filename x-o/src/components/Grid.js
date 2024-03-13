2f773d1e2c3a89253808ad32654421414ea36589
Jake Field <jacobprogrammes@gmail.com>
2024-03-13 11:49:17 +0000

    WinSequence branch init

--- x-o/src/components/Grid.js
+++ x-o/src/components/Grid.js
@@ -1,66 +1,11 @@
 import { useState } from "react";
 import { initialOXGrid } from "../App.js";
 import winLogic from "../winLogic.js";
-// each grid cell is an invisible button and displays <p>{initialOXGrid.value</p>
-// Upon button click, set value to either X / O, dictated by a function
-
-// create function to check for a win
-// export function winLogic(grid) {
-//   if (
-//     grid[0].value !== "" &&
-//     grid[0].value === grid[1].value &&
-//     grid[0].value === grid[2].value
-//   ) {
-//     console.log(`${grid[0].value} wins!`);
-//   } else if (
-//     grid[3].value !== "" &&
-//     grid[3].value === grid[4].value &&
-//     grid[3].value === grid[5].value
-//   ) {
-//     console.log(`${grid[3].value} wins!`);
-//   } else if (
-//     grid[6].value !== "" &&
-//     grid[6].value === grid[7].value &&
-//     grid[6].value === grid[8].value
-//   ) {
-//     console.log(`${grid[6].value} wins!`);
-//   } else if (
-//     grid[0].value !== "" &&
-//     grid[0].value === grid[3].value &&
-//     grid[0].value === grid[6].value
-//   ) {
-//     console.log(`${grid[0].value} wins!`);
-//   } else if (
-//     grid[1].value !== "" &&
-//     grid[1].value === grid[4].value &&
-//     grid[1].value === grid[7].value
-//   ) {
-//     console.log(`${grid[1].value} wins!`);
-//   } else if (
-//     grid[2].value !== "" &&
-//     grid[2].value === grid[5].value &&
-//     grid[2].value === grid[8].value
-//   ) {
-//     console.log(`${grid[2].value} wins!`);
-//   } else if (
-//     grid[0].value !== "" &&
-//     grid[0].value === grid[4].value &&
-//     grid[0].value === grid[8].value
-//   ) {
-//     console.log(`${grid[0].value} wins!`);
-//   } else if (
-//     grid[6].value !== "" &&
-//     grid[6].value === grid[4].value &&
-//     grid[6].value === grid[2].value
-//   ) {
-//     console.log(`${grid[6].value} wins!`);
-//   }
-// }
 
 export function Grid() {
   const [grid, setGrid] = useState(initialOXGrid);
   const [playerMove, setPlayerMove] = useState("X");
-
+  const [win, setWin] = useState(0);
   function inputXOrO(gridData) {
     // Clone the grid array to avoid directly mutating state
     const updatedGrid = [...grid];
@@ -101,3 +46,8 @@ function GridItem({ id, value, playerInput }) {
     </button>
   );
 }
+
+// init win state, pass down to winlogic as props
+// upon win, update win state in winLogic
+// useEffect hook listening for change in win state
+// when useEffect runs, render WinSequence comp
